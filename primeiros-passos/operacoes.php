c4035cd98268751a02199f3ccb7b03593a9fb0c5
renataeugenia <eugeniacomercial@gmail.com>
2022-07-13 16:44:05 -0300

    aula2

---
+++ primeiros-passos/operacoes.php
@@ -0,0 +1,20 @@
+<?php
+//$idade = 47;
+
+//$idadeDaqui10Anos = $idade + 10;
+
+//echo $idadeDaqui10Anos;
+
+//$idadeHa5Anos = $idade - 5;
+
+//echo $idadeHa5Anos;
+
+$soma = 2 + 3;
+$subtrai = 5 - 2;
+$multiplica = 5 * 7;
+$divide = 2/2;
+$doisAoCubo = 2 ** 3;
+$doisAoQuadradro = 2 ** 2;
+$restoDaDivisao = 10 % 3;
+
+echo $restoDaDivisao;
---
+++ primeiros-passos/tipos.php
@@ -0,0 +1,19 @@
+<?php
+// tipos primitivos:
+
+$idade = 21; // integer
+
+$salario = 1000.32; //double
+$divisao = 10/3;
+
+$texto = "Olá Mundo"; //string
+
+$verdadeiro = true; //boolean
+$falso = false; // boolean
+
+
+echo gettype($texto);
+
+/* php é uma linguagem dinamicamente tipada
+ não é precios indicar o tipo da variavel
+ela entende pelo valor.*/
